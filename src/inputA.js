67d22655e5f7496beb4c6f16e384ac7b7a9089c4
giri68 <giri.pdt@gmail.com>
2017-10-04 13:02:39 -0700

    empty input value and added date to the chat

--- src/inputA.js
+++ src/inputA.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './input.css';
 
 export default function Input(props){
+    const form = document.getElementById("inputA");
     return (
-        <form className="inputA" onSubmit={event => {props.onSubmit();
-            event.preventDefault()}}>
+        <form id='inputA' className="inputA" onSubmit={event => {props.onSubmit();
+            event.preventDefault();
+            form.reset()}}>
             <input type="text" placeholder="Type Here" name="inputA" onChange={event => props.onChange(event.target.value)} value={props.value}>
             </input>
             <label htmlFor="inputA">Giri</label>
